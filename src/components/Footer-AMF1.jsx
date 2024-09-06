d65f4bd43d9802dfac9d794a50d0af8369e76fed
Oyedolapo <oyedolapooyeniran@gmail.com>
2024-09-06 17:35:33 +0100

    First commit

---
+++ src/components/Footer-AMF1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-5  sm:px-10 px-5'></footer>
+  )
+}
+
+export default Footer
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://dfc9d7887c919f7bf5c5ddd2f6d75da5@o4507888587571200.ingest.us.sentry.io/4507888590848000",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
